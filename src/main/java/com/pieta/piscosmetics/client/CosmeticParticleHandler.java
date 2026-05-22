43a8fa6ac7793b52eca2839fb11dc986313e3cc9
Pi-Eta <alonepariah@gmail.com>
2026-05-22 13:20:00 +0700

    v1.1.2 Fixed registering animation controllers being in a common-side class, which was causing servers to crash.

--- src/main/java/com/pieta/piscosmetics/client/CosmeticParticleHandler.java
+++ src/main/java/com/pieta/piscosmetics/client/CosmeticParticleHandler.java
@@ -20,7 +20,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
-public class CosmeticParticleHandler_d {
+public class CosmeticParticleHandler {
 
     @SubscribeEvent
     public static void onClientTick(ClientTickEvent.Post event) {
